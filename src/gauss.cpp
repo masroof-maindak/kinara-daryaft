afe4f23b36f16e58fae0829fa2e75034cffefe19
masroof-maindak <mujtaba.asim.amin@gmail.com>
2025-09-25 23:24:09 +0500

    feat: gaussian filter generation

--- src/gauss.cpp
+++ src/gauss.cpp
@@ -1,43 +1,44 @@
-#include <cmath>
 #include <knr/gauss.h>
 
-#include <cstdint>
 #include <math.h>
+#include <stdint.h>
 
 int compute_filter_size(float sigma, float T) {
-    int size_half{static_cast<int>(sqrt(-log(T)) * 2 * sigma * sigma)};
-    return 2 * size_half + 1;
+    int half_size{static_cast<int>(sqrt(-log(T)) * 2 * sigma * sigma)};
+    return 2 * half_size + 1;
 }
 
 std::pair<cv::Mat, float> generate_gaussian_filter(const int filter_size, const float sigma) {
     cv::Mat filt_f{};
     filt_f.create(filter_size, filter_size, CV_32FC1);
 
+    const float two_sigma_sq{2 * sigma * sigma};
+    const int half_size{filter_size / 2};
+
+    double sum{};
     for (int y = 0; y < filter_size; y++) {
         float *filt_row = filt_f.ptr<float>(y);
-        for (int x = 0; x < filter_size; x++)
-            filt_row[x] = exp(-(x * x + y * y) / (2 * sigma * sigma));
+        for (int x = 0; x < filter_size; x++) {
+            int xx{std::abs(x - half_size)};
+            int yy{std::abs(y - half_size)};
+            filt_row[x] = (1 / std::numbers::pi * two_sigma_sq) * exp(-(xx * xx + yy * yy) / two_sigma_sq);
+            sum += filt_row[x];
+        }
     }
 
-    float min{}, max{}, scale_factor{};
-
-    // TODO: find minimum and maximum from filt_f to determine scaling factor
-
-    // TODO: copy over into filt_u8
+    filt_f /= sum;
 
     cv::Mat filt_u8{};
     filt_u8.create(filter_size, filter_size, CV_8UC1);
 
     for (int y = 0; y < filter_size; y++) {
-
-        std::uint8_t *filt_u8_row = filt_u8.ptr<std::uint8_t>(y);
         const float *filt_f_row   = filt_f.ptr<float>(y);
-
+        std::uint8_t *filt_u8_row = filt_u8.ptr<std::uint8_t>(y);
         for (int x = 0; x < filter_size; x++)
-            filt_u8_row[x] = filt_f_row[x] * scale_factor;
+            filt_u8_row[x] = static_cast<std::uint8_t>(filt_f_row[x] * 255);
     }
 
-    return {filt_u8, scale_factor};
+    return {filt_u8, sum};
 }
 
 std::pair<cv::Mat, cv::Mat> compute_partial_derivatives(const cv::Mat &gaussian_filt) {
